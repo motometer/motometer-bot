196c21de460fbe1326fb6700bc7c1437d1fbc82d
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-09-24 11:50:52 +0300

    Checkstyle plugin & configuration.

--- src/main/java/org/motometer/bot/MotometerBotApplication.java
+++ src/main/java/org/motometer/bot/MotometerBotApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MotometerBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MotometerBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MotometerBotApplication.class, args);
+    }
 }
