d5654b4d9ff1dd33dcd1df7e47a0341bdad65062
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-09-27 21:46:29 +0300

    Initial commit.

---
+++ src/main/java/org/motometer/bot/motometerbot/MotometerBotApplication.java
@@ -0,0 +1,13 @@
+package org.motometer.bot.motometerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotometerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotometerBotApplication.class, args);
+	}
+
+}
