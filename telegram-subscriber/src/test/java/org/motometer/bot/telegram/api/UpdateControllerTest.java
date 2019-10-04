009c3e8fab987ae7729463f8090c7904d6630448
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-04 11:28:18 +0300

    Kafka with test containers.

--- telegram-subscriber/src/test/java/org/motometer/bot/telegram/api/UpdateControllerTest.java
+++ telegram-subscriber/src/test/java/org/motometer/bot/telegram/api/UpdateControllerTest.java
@@ -3,16 +3,12 @@ package org.motometer.bot.telegram.api;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.motometer.bot.telegram.kafka.KafkaTestListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
@@ -25,8 +21,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.net.URI;
 import java.nio.charset.Charset;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,9 +36,6 @@ class UpdateControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Autowired
-    private CompletableFuture<String> resultFuture;
-
     @Test
     void onUpdate() throws Exception {
         String update = IOUtils.resourceToString("/org/motometer/bot/telegram/api/UpdateControllerTest/update.json", Charset.defaultCharset());
@@ -56,21 +47,6 @@ class UpdateControllerTest {
         ResponseEntity<Void> response = restTemplate.exchange(request, Void.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        String resultMessage = resultFuture.get(3, TimeUnit.SECONDS);
-        assertThat(resultMessage).isNotNull();
-    }
-
-    @TestConfiguration
-    static class Config {
-        @Bean
-        public CompletableFuture<String> resultFuture() {
-            return new CompletableFuture<>();
-        }
-
-        @Bean
-        public KafkaTestListener kafkaTestListener() {
-            return new KafkaTestListener(resultFuture());
-        }
     }
 
     static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
