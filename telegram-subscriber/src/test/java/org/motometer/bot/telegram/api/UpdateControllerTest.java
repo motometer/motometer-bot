5d0c3d93d45a02553c67b9757c7d7c8ec9584be3
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-04 21:44:24 +0300

    Kafka integration. (#6)
    Add kafka integration.
    Add docker compose for kafka bootstrap.
    Add testcontainers test.
    Integration source set to be extracted later.

--- telegram-subscriber/src/test/java/org/motometer/bot/telegram/api/UpdateControllerTest.java
+++ telegram-subscriber/src/test/java/org/motometer/bot/telegram/api/UpdateControllerTest.java
@@ -5,29 +5,39 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Testcontainers
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = {UpdateControllerTest.Initializer.class})
 class UpdateControllerTest {
+
+    @Container
+    public static KafkaContainer kafka = new KafkaContainer();
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    void onUpdate() throws IOException, URISyntaxException {
+    void onUpdate() throws Exception {
         String update = IOUtils.resourceToString("/org/motometer/bot/telegram/api/UpdateControllerTest/update.json", Charset.defaultCharset());
 
         RequestEntity<String> request = RequestEntity.post(new URI(restTemplate.getRootUri() + "/bot/update/fake-token"))
@@ -37,5 +47,13 @@ class UpdateControllerTest {
         ResponseEntity<Void> response = restTemplate.exchange(request, Void.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                "kafka.bootstrapAddress=localhost:" + kafka.getMappedPort(9093)
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
     }
 }
