b1d036ee0ce596d1d201305d88f180b5284bb12f
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-05 17:32:03 +0300

    Telegram publisher.

--- telegram-publisher/src/main/java/org/motometer/bot/telegram/core/Processor.java
+++
@@ -1,6 +0,0 @@
-package org.motometer.bot.telegram.core;
-
-public class Processor {
-
-    @StreamListener("input")
-}
--- telegram-subscriber/src/main/java/org/motometer/bot/telegram/kafka/Config.java
+++ telegram-subscriber/src/main/java/org/motometer/bot/telegram/kafka/Config.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.motometer.telegram.bot.api.Update;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,16 +36,16 @@ public class Config {
     }
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, Update> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, Update> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
